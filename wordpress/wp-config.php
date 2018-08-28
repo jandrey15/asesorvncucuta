cacd58f3d731ab4fd6a176c53a31cef20133bde1
john andrey <andrey_201015@hotmail.com>
2018-08-28 16:01:01 -0500

    Puliendo detalles del nginx

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -37,6 +37,8 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+// define ('WP_MEMORY_LIMIT', '256M');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
