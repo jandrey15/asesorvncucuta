7d944bb6fdb1527bf090ec7699345be1b52a788c
John Serrano <andrey_201015@hotmail.com>
2018-09-05 14:44:56 -0500

    Agregando campo de galeria

--- wordpress/wp-content/themes/twentyfifteen-child/functions.php
+++ wordpress/wp-content/themes/twentyfifteen-child/functions.php
@@ -61,47 +61,47 @@ add_action( 'init', 'add_role_viajero' ); */
 
 // add_action( 'init', 'nuevos_init' );
 
-// function usados_init() {
-//   $labels = array(
-//     'name'              => _x( 'Usados', 'post type general name', 'your-plugin-textdomain' ),
-//     'singular_name'     => _x( 'Usados', 'post type general name', 'your-plugin-textdomain' ),
-//     'menu_name'         => _x( 'Carros usados', 'admin menu', 'your-plugin-textdomain' ),
-//     'name_admin_bar'    => _x( 'Usados', 'add new on admin bar', 'your-plugin-textdomain' ),
-//     'add_new'           => _x( 'Añadir carro', 'carro', 'your-plugin-textdomain' ),
-//     'add_new_item'      => __( 'Añadir carro usado', 'your-plugin-textdomain' ),
-//     'new_item'          => __( 'Carro usado', 'your-plugin-textdomain' ),
-//     'edit_item'         => __( 'Editar carro', 'your-plugin-textdomain' ),
-//     'view_item'         => __( 'Ver carro', 'your-plugin-textdomain' ),
-//     'all_items'         => __( 'Todos los carros', 'your-plugin-textdomain' ),
-//     'search_items'      => __( 'Buscar carros', 'your-plugin-textdomain' ),
-//     'parent_item_colon' => __( 'Carros padre', 'your-plugin-textdomain' ),
-//     'not_found'         => __( 'No hemos encontrado carros.', 'your-plugin-textdomain' ),
-//     'not_found_in_trash'=> __( 'No hemos encontrado carros en la papelera', 'your-plugin-textdomain' ),
-//   );
+function articulo_init() {
+  $labels = array(
+    'name'              => _x( 'Artículos', 'post type general name', 'your-plugin-textdomain' ),
+    'singular_name'     => _x( 'Artículos', 'post type general name', 'your-plugin-textdomain' ),
+    'menu_name'         => _x( 'Artículos', 'admin menu', 'your-plugin-textdomain' ),
+    'name_admin_bar'    => _x( 'Artículos', 'add new on admin bar', 'your-plugin-textdomain' ),
+    'add_new'           => _x( 'Añadir artículo', 'artículo', 'your-plugin-textdomain' ),
+    'add_new_item'      => __( 'Añadir artículo usado', 'your-plugin-textdomain' ),
+    'new_item'          => __( 'Nuevo artículo', 'your-plugin-textdomain' ),
+    'edit_item'         => __( 'Editar artículo', 'your-plugin-textdomain' ),
+    'view_item'         => __( 'Ver artículo', 'your-plugin-textdomain' ),
+    'all_items'         => __( 'Todos los artículos', 'your-plugin-textdomain' ),
+    'search_items'      => __( 'Buscar artículos', 'your-plugin-textdomain' ),
+    'parent_item_colon' => __( 'Artículos padre', 'your-plugin-textdomain' ),
+    'not_found'         => __( 'No hemos encontrado artículos.', 'your-plugin-textdomain' ),
+    'not_found_in_trash'=> __( 'No hemos encontrado artículos en la papelera', 'your-plugin-textdomain' ),
+  );
 
-//   $args = array(
-//     'labels'            => $labels,
-//     'description'       => __('Description', 'your-plugin-textdomain'),
-//     'taxonomies'        => array('category'),
-//     'public'            => true,
-//     'public_queryable'  => true,
-//     'show_ui'           => true,
-//     'show_in_menu'      => true,
-//     'query_var'         => true,
-//     'rewrite'           => array( 'slug' => 'usados' ),
-//     'capability_type'   => 'post',
-//     'has_archive'       => true,
-//     'hierarchical'      => false,
-//     'menu_position'     => null,
-//     'show_in_rest'      => true,
-//     'menu_icon'         => 'dashicons-welcome-write-blog',
-//     'supports'          => array( 'title', 'editor', 'author', 'thumbnail' )
-//   );
+  $args = array(
+    'labels'            => $labels,
+    'description'       => __('Description', 'your-plugin-textdomain'),
+    'taxonomies'        => array('category'),
+    'public'            => true,
+    'public_queryable'  => true,
+    'show_ui'           => true,
+    'show_in_menu'      => true,
+    'query_var'         => true,
+    'rewrite'           => array( 'slug' => 'articulo' ),
+    'capability_type'   => 'post',
+    'has_archive'       => true,
+    'hierarchical'      => false,
+    'menu_position'     => null,
+    'show_in_rest'      => true,
+    'menu_icon'         => 'dashicons-welcome-write-blog',
+    'supports'          => array( 'title', 'editor', 'author', 'thumbnail' )
+  );
 
-//   register_post_type( 'usados', $args );
-// }
+  register_post_type( 'articulo', $args );
+}
 
-// add_action( 'init', 'usados_init' );
+add_action( 'init', 'articulo_init' );
 
 
 // Función para crear una taxonomía
@@ -333,6 +333,9 @@ function register_custom_fields() {
         array( 'get_callback' => 'show_fields')
     );
     register_rest_field('post', 'precio',
+        array( 'get_callback' => 'show_fields')
+    );
+    register_rest_field('post', 'galeria',
         array( 'get_callback' => 'show_fields')
     );
 }
