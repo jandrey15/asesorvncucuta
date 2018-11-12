c7f849a99deb86c0c02fe94311b48248373e9639
john andrey <andrey_201015@hotmail.com>
2018-11-12 16:32:57 -0500

    Agregando terraform y agregando deployments/prod

---
+++ infra/main.tf
@@ -0,0 +1,25 @@
+resource "digitalocean_floating_ip" "web" {
+  region            = "nyc1"
+}
+
+resource "digitalocean_tag" "webtag" {
+  name = "web-site"
+}
+
+resource "digitalocean_droplet" "web" {
+  name               = "asesorvncucuta-test-terraform"
+  size               = "s-1vcpu-1gb"
+  image              = "${var.image_id}"
+  region             = "nyc1"
+  tags               = ["${digitalocean_tag.webtag.id}"]
+  lifecycle {
+    create_before_destroy = true
+  }
+  ssh_keys           = [20843332, 21489207],
+  user_data          = "${file("web.conf")}"
+}
+
+resource "digitalocean_floating_ip_assignment" "web" {
+  ip_address = "${digitalocean_floating_ip.web.id}"
+  droplet_id = "${digitalocean_droplet.web.id}"
+}
