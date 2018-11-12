7fc790d00a5a3068439cca4073a92edc7694aa99
john andrey <andrey_201015@hotmail.com>
2018-11-11 20:56:51 -0500

    Cambios en qa

--- deployments/qa.sh
+++ deployments/qa.sh
@@ -1,10 +1,12 @@
 npm install -g now
-pwd
+echo '--------------------------------'
+cd web
 echo "deploying..."
-
-now secrets add listid $LISTID -t $now_token
-now secrets add api_key $API_KEY -t $now_token
-now secrets add secret_captcha $SECRET_CAPTCHA -t $now_token
+# Error! The secret name "listid" already exists for this user
+# now secrets add listid $LISTID -t $now_token
+# now secrets add api_key $API_KEY -t $now_token
+# now secrets add secret_captcha $SECRET_CAPTCHA -t $now_token
+#Tener en cuenta que una ves se agg el secret queda en el user
 
 URL=$(now -t $now_token)
 echo "running acceptance on $URL"
