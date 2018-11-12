bb4cb609ae83438c79e4d185703568583d9236d2
john andrey <andrey_201015@hotmail.com>
2018-11-12 18:00:21 -0500

    Cambiando unzip packer

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,16 +1,16 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
 # wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
 
-sudo rm -r /usr/local/bin/packer
-sudo unzip /tmp/packer.zip -d /usr/local/bin/
+# sudo rm -r /usr/local/bin/packer
+# sudo unzip /tmp/packer.zip -d /usr/local/bin/
+sudo unzip -o /tmp/packer.zip -d /usr/local/bin/
 echo '--------------------'
-pwd
-ls
 # unzip /tmp/terraform.zip -d ~/bin
 
 
-packer validate deployments/template.json &&
-packer build deployments/template.json &&
+packer validate ./deployments/template.json
+packer validate deployments/template.json
+# packer build deployments/template.json &&
 
 # export TF_VAR_image_id=$(curl -X GET -H "Content-Type: application/json" -H "Authorization: Bearer $DIGITALOCEAN_API_TOKEN" "https://api.digitalocean.com/v2/images?private=true" | jq ."images[] | select(.name == \"devops-demo-$TRAVIS_BUILD_NUMBER\") | .id") &&
 
