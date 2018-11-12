31bce727c01b5c9404d4679db9ff1a9f4c7a4507
john andrey <andrey_201015@hotmail.com>
2018-11-12 17:49:38 -0500

    Cambiando unzip packer

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -1,7 +1,7 @@
 wget -O /tmp/packer.zip https://releases.hashicorp.com/packer/1.3.2/packer_1.3.2_linux_amd64.zip
 # wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/0.11.10/terraform_0.11.10_linux_amd64.zip
 packer --version
-rm -r /usr/local/bin/packer
+sudo rm -r /usr/local/bin/packer
 unzip /tmp/packer.zip -d /usr/local/bin/
 packer --version
 # unzip /tmp/packer.zip -d ~/bin
