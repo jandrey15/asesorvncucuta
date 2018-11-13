1a3b1857b45ac905918335bf1fb5d4e4a9c3c893
John Serrano <andrey_201015@hotmail.com>
2018-11-13 16:51:17 -0500

    Mas cambios deployment prod bash

--- deployments/prod.sh
+++ deployments/prod.sh
@@ -26,7 +26,7 @@ upload_files() {
   git remote rm origin &&
   # Add new "origin" with access token in the git URL for authentication
   git remote add origin https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
-  git add test.txt && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]"
+  git add test.txt && git commit -m "Deployed $TRAVIS_BUILD_NUMBER" -m "[skip ci]"&&
   git status
   echo "Se agrego la key remote"
   git remote -v
