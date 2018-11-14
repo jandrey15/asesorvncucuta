40ef3ac9cf50420156e1482262c587f49fc2d869
John Serrano <andrey_201015@hotmail.com>
2018-11-14 14:42:41 -0500

    Cambios en travis push git

---
+++ deployments/.travis-push.sh
@@ -0,0 +1,42 @@
+setup_git() {
+  git config --global user.email "build@travis-ci.com" &&
+  git config --global user.name "Travis CI" &&
+}
+
+commit_country_json_files() {
+  git checkout master &&
+  git status &&
+  git add infra/test.txt && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]" &&
+  # git add infra && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]"
+}
+
+upload_files() {
+  git remote rm origin &&
+  git remote add origin https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
+  git push origin master --quiet > /dev/null 2>&1
+}
+
+# git add test.txt && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]"
+
+
+# upload_files() {
+#   # git remote rm origin
+#   # # Add new "origin" with access token in the git URL for authentication
+#   # git remote add origin https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
+#   git remote add deploy https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
+#   git push deploy master --quiet > /dev/null 2>&1
+#   # git push origin master --quiet > /dev/null 2>&1
+#   # git push origin master
+#   echo "push a master"
+# }
+
+setup_git
+
+commit_country_json_files
+
+if [ $? -eq 0 ]; then
+  echo "A new commit with changed country JSON files exists. Uploading to GitHub"
+  upload_files
+else
+  echo "No changes in country JSON files. Nothing to do"
+fi
--- deployments/prod.sh
+++ deployments/prod.sh
@@ -15,34 +15,11 @@ echo $TF_VAR_image_id &&
 cd infra &&
 # terraform init -input=false && 
 # terraform apply -input=false -auto-approve && cd .. &&
-git config --global user.email "build@travis-ci.com" &&
-git config --global user.name "Travis CI" &&
+
 # git add infra && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]"
 touch test.txt &&
 echo 'Hola mundo' >> test.txt 
-# git add test.txt && git commit -m "Deployed $TRAVIS_BUILD_NUMBER [skip ci]"
 
-
-# upload_files() {
-#   # git remote rm origin
-#   # # Add new "origin" with access token in the git URL for authentication
-#   # git remote add origin https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
-#   git remote add deploy https://jandrey15:${GH_TOKEN}@github.com/jandrey15/asesorvncucuta.git > /dev/null 2>&1
-#   git push deploy master --quiet > /dev/null 2>&1
-#   # git push origin master --quiet > /dev/null 2>&1
-#   # git push origin master
-#   echo "push a master"
-# }
-
-# # Attempt to commit to git only if "git commit" succeeded
-# if [ $? -eq 0 ]; then
-#   echo "A new commit with changed country JSON files exists. Uploading to GitHub"
-#   upload_files
-# else
-#   echo "No changes in country JSON files. Nothing to do"
-# fi
-
-# git push origin master &&
 echo "Deployed and saved!"
 # echo "Deployed and saved!" &&
 # echo "Now deleting the image previously created" &&
