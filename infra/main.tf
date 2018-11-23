68cc769a12d7bb9f721b5df52b10c3279efe3cb4
John Serrano <andrey_201015@hotmail.com>
2018-11-23 16:30:02 -0500

    Cambiando infra terraform

--- infra/main.tf
+++ infra/main.tf
@@ -7,26 +7,34 @@ resource "digitalocean_tag" "webtag" {
 }
 
 resource "digitalocean_droplet" "web" {
-  name               = "asesorvncucuta-test-terraform"
-  size               = "s-1vcpu-1gb"
-  image              = "${var.image_id}"
-  region             = "nyc1"
-  tags               = ["${digitalocean_tag.webtag.id}"]
+  name   = "asesorvncucuta-test-terraform"
+  size   = "s-1vcpu-1gb"
+  image  = "${var.image_id}"
+  region = "nyc1"
+  tags   = ["${digitalocean_tag.webtag.id}"]
+
   lifecycle {
     create_before_destroy = true
   }
+
   provisioner "local-exec" {
-    command = "sleep 160 && curl ${self.ipv4_address}"
+    command = "sleep 120 && curl ${self.ipv4_address}"
   }
-  ssh_keys           = [20843332, 21489207],
-  # user_data          = "${file("web.conf")}"
+
+  ssh_keys = [20843332, 21489207]
+
+  user_data = "${file("web.conf")}"
 }
 
 resource "digitalocean_floating_ip_assignment" "web-api" {
   depends_on = ["digitalocean_droplet.web"]
-  ip_address = "178.128.133.22"
+
+  # ip_address = "178.128.133.22"
+  ip_address = "104.248.108.198"
+
   lifecycle {
     create_before_destroy = true
   }
+
   droplet_id = "${digitalocean_droplet.web.id}"
 }
